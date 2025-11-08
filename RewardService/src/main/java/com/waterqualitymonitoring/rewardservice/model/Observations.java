bc499f8a5897fad3738bf26b1af0e8fc87dcf1c8
Nadeem Shaik <nademshaik0513@gamil.com>
2025-11-08 17:48:24 +0530

    Observations enum

--- RewardService/src/main/java/com/waterqualitymonitoring/rewardservice/model/Observations.java
+++ RewardService/src/main/java/com/waterqualitymonitoring/rewardservice/model/Observations.java
@@ -3,5 +3,9 @@ package com.waterqualitymonitoring.rewardservice.model;
 public enum Observations {
     CLEAR,
     MURKY,
-    PRESENCE_OF_ODOUR
+    PRESENCE_OF_ODOUR,
+    CLOUDY,
+    FOAMY,
+    OILY,
+    DISCOLORED;
 }
