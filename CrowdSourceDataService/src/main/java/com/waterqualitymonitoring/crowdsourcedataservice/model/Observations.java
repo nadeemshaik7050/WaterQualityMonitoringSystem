d235364551e8b12ea408c858fbc56c9b36d91efc
Nadeem Shaik <nademshaik0513@gamil.com>
2025-11-08 17:06:32 +0530

    integration changes

--- CrowdSourceDataService/src/main/java/com/waterqualitymonitoring/crowdsourcedataservice/model/Observations.java
+++ CrowdSourceDataService/src/main/java/com/waterqualitymonitoring/crowdsourcedataservice/model/Observations.java
@@ -3,5 +3,9 @@ package com.waterqualitymonitoring.crowdsourcedataservice.model;
 public enum Observations {
     CLEAR,
     MURKY,
-    PRESENCE_OF_ODOUR
+    PRESENCE_OF_ODOUR,
+    CLOUDY,
+    FOAMY,
+    OILY,
+    DISCOLORED;
 }
